46f2c7eb0eabe9b93ede0a8455bb47dc6514d279
Christian Slabbekoorn <cslabbekoorn@ptc.com>
2020-01-07 16:46:59 +0100

    Added game states and events to keep track of these in other scripts

---
+++ Secrets of Emeth/Assets/Scripts/Controllers/GameManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Gamestates GameState;
+
+    public delegate void GameStateChanged(Gamestates currentGameState, Gamestates newGameState);
+    public static event GameStateChanged OnGameStateChanged;
+
+    void Start()
+    {
+        ChangeGameState(Gamestates.MAIN_MENU);
+    }
+    
+    void Update()
+    {
+        
+        
+    }
+
+    public void ChangeGameState(Gamestates newGameState)
+    {
+        Gamestates previousGameState = GameState;
+        GameState = newGameState;
+        OnGameStateChanged(previousGameState, GameState);
+    }
+}
+
+public enum Gamestates
+{
+    MAIN_MENU,
+    PAUSE_MENU,
+    INTERACTING,
+    OVERWORLD,
+    INTERIOR,
+    IN_BATTLE
+}
